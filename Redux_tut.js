bc0b2761f41b0011d2a924b3616d4c9d8b087027
krgarav <krgarav@gmail.com>
2023-04-19 05:37:20 +0530

    Added files

---
+++ Redux_tut.js
@@ -0,0 +1,22 @@
+const redux = require("redux");
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "DECREMENT") {
+    return { counter: state.counter - 1 };
+  }
+  return {
+    counter: state.counter + 1,
+  };
+};
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "DECREMENT" });
